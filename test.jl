13ae892c299af1a62c69dbccab33b188f46ce78d
bradleywjenks <b.jenks21@imperial.ac.uk>
2024-05-13 12:51:38 +0100

    added mass transfer computation for each hydraulic time step

--- test.jl
+++ test.jl
@@ -62,6 +62,7 @@ V_tk = Matrix(lev_tk .* repeat(network.tank_area, 1, n_t)')'
 # set discretization parameters and variables
 Δt = 5 # 60 seconds
 L_p = network.L[network.pipe_idx]
+D_p = network.D[network.pipe_idx]
 vel_p_max = maximum(vel_p, dims=2)
 s_p = floor.(Int, L_p ./ (vel_p_max .* Δt))
 s_p[s_p .== 0] .= 1
@@ -76,27 +77,31 @@ T = (sim_results.timestamp[end] + (sim_results.timestamp[end] - sim_results.time
 T_k = Int(T ./ Δt) # number of discrete time steps
 source_cl = repeat([1.5], network.n_r)
 
-c_r0 = repeat(source_cl, 1)
-c_j0 = zeros(network.n_j)
-c_tk0 = zeros(network.n_tk)
-c_m0 = zeros(network.n_m)
-c_v0 = zeros(network.n_v)
-c_p0 = zeros(n_s)
-x0 = vcat(c_r0, c_j0, c_tk0, c_m0, c_v0, c_p0)
+c_r_t = repeat(source_cl, 1)
+c_j_t = zeros(network.n_j)
+c_tk_t = zeros(network.n_tk)
+c_m_t = zeros(network.n_m)
+c_v_t = zeros(network.n_v)
+c_p_t = zeros(n_s)
+x_t = vcat(c_r_t, c_j_t, c_tk_t, c_m_t, c_v_t, c_p_t)
 
 # set chlorine decay constants
-kb = (-0.5/3600/24) # units = 1/second
-kw = (-0.1/3600/24) # units = 1/second
+kb = (0.5/3600/24) # units = 1/second
+kw = (0.1/3600/24) # units = 1/second
 
 ### construct coefficient matrices across all hydraulic time steps ###
 n_x = network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + n_s
-kt = 1 # iterate through hydraulic time steps later
-kt_Δt = 1 # iterate through hydraulic time steps later
+n_u = 1 # control actuator inputs (TO BE COMPLETED)
+k_t = 1 # iterate through hydraulic time steps later
+k_t_Δt = 1 # iterate through hydraulic time steps later
 
-# Ex(t+Δt) = Ax(t) + Bu(t)
+u_t = zeros(n_u)
+
+# Ex(t+Δt) = Ax(t) + Bu(t) + f(x(t))
 E = spzeros(n_x, n_x)
 A = spzeros(n_x, n_x)
-# ignore B matrix and u vector for now
+B = spzeros(n_x, n_u)
+f = spzeros(n_x, n_x)
 
 # construct reservoir coefficient matrices
 for r ∈ 1:network.n_r
@@ -108,8 +113,8 @@ end
 for (i, j) ∈ enumerate(network.junction_idx)
 
     # find all incoming and outgoing link indices at junction j
-    I_in = findall(x -> x == 1, A_i[:, j, kt_Δt]) # set of incoming links at junction j
-    I_out = findall(x -> x == -1, A_i[:, j, kt_Δt]) # set of outgoing links at junction j
+    I_in = findall(x -> x == 1, A_i[:, j, k_t_Δt]) # set of incoming links at junction j
+    I_out = findall(x -> x == -1, A_i[:, j, k_t_Δt]) # set of outgoing links at junction j
 
     # assign c_j(t+Δt) matrix coefficients
     E[network.n_r + i, network.n_r + i] = 1.0
@@ -119,14 +124,14 @@ for (i, j) ∈ enumerate(network.junction_idx)
     for link_idx ∈ I_in
         if link_idx ∈ network.pump_idx
             idx = findfirst(x -> x == link_idx, network.pump_idx)
-            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + idx] = -q_m[idx, kt_Δt] ./ (d[j, kt_Δt] + sum(q[I_out, kt_Δt]))
+            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + idx] = -q_m[idx, k_t_Δt] ./ (d[j, k_t_Δt] + sum(q[I_out, k_t_Δt]))
         elseif link_idx ∈ network.valve_idx
             idx = findfirst(x -> x == link_idx, network.valve_idx)
-            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + network.n_m + idx] = -q_v[idx, kt_Δt] ./ (d[j, kt_Δt] + sum(q[I_out, kt_Δt]))
+            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + network.n_m + idx] = -q_v[idx, k_t_Δt] ./ (d[j, k_t_Δt] + sum(q[I_out, k_t_Δt]))
         elseif link_idx ∈ network.pipe_idx
             idx = findfirst(x -> x == link_idx, network.pipe_idx)
             Δs = sum(s_p[1:idx])
-            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs] = -q_p[idx, kt_Δt] ./ (d[j, kt_Δt] + sum(q[I_out, kt_Δt]))
+            E[network.n_r + i, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs] = -q_p[idx, k_t_Δt] ./ (d[j, k_t_Δt] + sum(q[I_out, k_t_Δt]))
         else 
             @error "Link index not found in network pipe, pump, or valve indices."
         end
@@ -139,31 +144,34 @@ end
 for (i, tk) ∈ enumerate(network.tank_idx)
 
     # find all incoming and outgoing link indices at junction j
-    I_in = findall(x -> x == 1, A_i[:, tk, kt]) # set of incoming links at junction j
-    I_out = findall(x -> x == -1, A_i[:, tk, kt]) # set of outgoing links at junction j
+    I_in = findall(x -> x == 1, A_i[:, tk, k_t]) # set of incoming links at junction j
+    I_out = findall(x -> x == -1, A_i[:, tk, k_t]) # set of outgoing links at junction j
 
     # assign c_tk(t+Δt) matrix coefficients
     E[network.n_r + network.n_j + i, network.n_r + network.n_j + i] = 1.0
 
     # assign c_tk(t) matrix coefficients
-    A[network.n_r + network.n_j + i, network.n_r + network.n_j + i] = (V_tk[i, kt] + (kb .* V_tk[i, kt] - sum(q[I_out, kt])) .* Δt) ./ V_tk[i, kt_Δt]
+    A[network.n_r + network.n_j + i, network.n_r + network.n_j + i] = (V_tk[i, k_t] + - sum(q[I_out, k_t]) .* Δt) ./ V_tk[i, k_t_Δt]
 
     # assign c_link(t) matrix coefficients
     for link_idx ∈ I_in
         if link_idx ∈ network.pump_idx
             idx = findfirst(x -> x == link_idx, network.pump_idx)
-            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + idx] = q_m[idx, kt] ./ V_tk[i, kt_Δt]
+            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + idx] = q_m[idx, k_t] ./ V_tk[i, k_t_Δt]
         elseif link_idx ∈ network.valve_idx
             idx = findfirst(x -> x == link_idx, network.valve_idx)
-            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + network.n_m + idx] = q_v[idx, kt] ./ V_tk[i, kt_Δt]
+            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + network.n_m + idx] = q_v[idx, k_t] ./ V_tk[i, k_t_Δt]
         elseif link_idx ∈ network.pipe_idx
             idx = findfirst(x -> x == link_idx, network.pipe_idx)
             Δs = sum(s_p[1:idx])
-            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs] = q_p[idx, kt] ./ V_tk[i, kt_Δt]
+            A[network.n_r + network.n_j + i, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs] = q_p[idx, k_t] ./ V_tk[i, k_t_Δt]
         else 
             @error "Link index not found in network pipe, pump, or valve indices."
         end
     end
+
+    # assign decay coefficient in f for tank tk
+    f[network.n_r + network.n_j + i, network.n_r + network.n_j + i] = kb * c_tk_t[i] * V_tk[i, k_t] * Δt * -1
 
 end
 
@@ -172,7 +180,7 @@ end
 for (i, m) ∈ enumerate(network.pump_idx)
 
     # assign c_node(t+Δt) matrix coefficients
-    node_out = findall(x -> x == -1, A_i[m, :, kt_Δt])[1]
+    node_out = findall(x -> x == -1, A_i[m, :, k_t_Δt])[1]
     if node_out ∈ network.reservoir_idx
         idx = findfirst(x -> x == node_out, network.reservoir_idx)
         E[network.n_r + network.n_j + network.n_tk + i, idx] = -1.0
@@ -196,7 +204,7 @@ end
 for (i, v) ∈ enumerate(network.valve_idx)
 
     # assign c_node(t+Δt) matrix coefficients
-    node_out = findall(x -> x == -1, A_i[v, :, kt_Δt])[1]
+    node_out = findall(x -> x == -1, A_i[v, :, k_t_Δt])[1]
     if node_out ∈ network.reservoir_idx
         idx = findfirst(x -> x == node_out, network.reservoir_idx)
         E[network.n_r + network.n_j + network.n_tk + network.n_m + i, idx] = -1.0
@@ -217,19 +225,29 @@ end
 
 
 # construct pipe coefficient matrices (start with Lax-Wendroff discretization scheme)
-λ_p1 = 0.5 .* λ_p[:, kt] .* (1 .+ λ_p[:, kt])
-λ_p2 = (1 .- λ_p[:, kt]).^2
-λ_p3 = -0.5 .* λ_p[:, kt] .* (1 .- λ_p[:, kt])
+λ_p1 = 0.5 .* λ_p[:, k_t] .* (1 .+ λ_p[:, k_t])
+λ_p2 = (1 .- λ_p[:, k_t]).^2
+λ_p3 = -0.5 .* λ_p[:, k_t] .* (1 .- λ_p[:, k_t])
+
+# mass transfer coefficient data
+ν = 1.0533e-6 # in m^2/s
+D_m = 1.208e-9 # @ 20°C in m^2/s
+Sc = ν / D_m
+Re = (4 .* q_p[:, k_t]) ./ (π .* D_p .* ν)
 
 for (i, p) ∈ enumerate(network.pipe_idx)
+
+    # compute mass transfer coefficient for pipe p (based on EPANET manual first-order decay model)
+    if Re[i] < 2000
+        Sh = 3.65 + (0.0668 * (D_p[i]/L_p[i]) * Re[i] * Sc) /  (1 + 0.04 * ((D_p[i]/L_p[i]) * Re[i] * Sc)^(2/3))
+    else
+        Sh = 0.0149 * Re[i]^0.88 * Sc^(1/3)
+    end
+    kf = Sh *(D_m / D_p[i])
 
     # find upstream and downstream node indices at pipe p
     node_in = findall(x -> x == 1, A_i[p, :, kt])[1]
     node_out = findall(x -> x == -1, A_i[p, :, kt])[1]
-
-    # compute decay constant for pipe p
-    # insert code here to compute  mass transfer coefficient...
-    k_p = kb + kw
 
     for s ∈ 1:s_p[i]
 
@@ -237,7 +255,11 @@ for (i, p) ∈ enumerate(network.pipe_idx)
 
         # assign C_pipe(t+Δt) matrix coefficients
         E[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = 1.0
-        
+
+        # assign decay term in f for pipe p
+        k_p = (kb + (4 * kw * kf * c_p_t[Δs + s]) / (D_p[i] * (kw + kf)))
+        f[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = k_p * c_p_t[Δs + s] * Δt * -1
+
         # assign C_pipe(t) and C_juction(t) matrix coefficients
         if s == 1
 
@@ -256,7 +278,7 @@ for (i, p) ∈ enumerate(network.pipe_idx)
             end
 
             # current segment s matrix coefficients at time t
-            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i] + k_p .* Δt
+            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i]
 
             # next segment s+1 matrix coefficients at time t
             if s_p[i] == 1
@@ -282,7 +304,7 @@ for (i, p) ∈ enumerate(network.pipe_idx)
             A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s - 1] = λ_p1[i]
 
             # current segment s matrix coefficients at time t
-            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i] + k_p .* Δt
+            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i]
 
             # next segment s+1 matrix coefficients at time t
             if node_in ∈ network.reservoir_idx
@@ -304,7 +326,7 @@ for (i, p) ∈ enumerate(network.pipe_idx)
             A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s - 1] = λ_p1[i]
 
             # current segment s matrix coefficients at time t
-            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i] + k_p .* Δt
+            A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s] = λ_p2[i]
 
             # next segment s+1 matrix coefficients at time t
             A[network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s, network.n_r + network.n_j + network.n_tk + network.n_m + network.n_v + Δs + s + 1] = λ_p3[i]
@@ -316,14 +338,16 @@ end
 
 
 
-# form system of linear equations: Ex(t+Δt) = Ax(t) + Bu(t)
-A_xt = A * x0
-prob = LinearProblem(E, A_xt)
+# form system of linear equations: Ex(t+Δt) = Ax(t) + Bu(t) + f(x(t))
+A_x_t = A * x_t
+B_u_t = B * u_t
+f_x_t = f * x_t
 cpu_time = @elapsed begin
+    # prob = LinearProblem(E, A_x_t + B_u_t + f_x_t)
     # xt_Δt = solve(prob, MKLPardisoFactorize()).u
     # linsolve = init(prob)
     # xt_Δt = solve(linsolve)
-    xt_Δt = E \ A_xt
+    x_t_Δt = E \ (A_x_t + B_u_t + f_x_t)
 end
 
 
